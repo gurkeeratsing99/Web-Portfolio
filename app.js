7978f2e7e46cd3abc725b0029edd4758af00c944
unknown <gurkeeratsingh99@gmail.com>
2022-07-22 02:29:20 -0700

    make it better

--- app.js
+++ app.js
@@ -13,41 +13,41 @@ app.get("/" , function(req,res){
 
 });
 
-// app.post("/send", function(req,res){
-//
-//   const name= req.body.name;
-//   const email = req.body.email;
-//   const reply =  req.body.reply;
-//
-//   const transporter = nodemailer.createTransport({
-//
-//       service : "hotmail",
-//       auth: {
-//         user: "gs2422163@gmail.com",
-//         pass: "Brarsingh=99"
-//       }
-//
-//
-//     });
-//
-//     const options={
-//       from : "gs2422163@gmail.com",
-//       to :"gurkeeratsingh99@gmail.com",
-//       subject:"You got question from "+name+ ", Email address: "+email,
-//       text:reply
-//     };
-//
-//     transporter.sendMail(options , function(err, info){
-//       if(err){
-//         console.log(err);
-//         return;
-//       }
-//       console.log("Sent : " + info.response);
-//     });
-//
-// res.sendFile(__dirname+"/index.html");
-//
-// });
+app.post("/send", function(req,res){
+
+  const name= req.body.name;
+  const email = req.body.email;
+  const reply =  req.body.reply;
+
+  const transporter = nodemailer.createTransport({
+
+      service : "hotmail",
+      auth: {
+        user: "gs2422163@gmail.com",
+        pass: "Brarsingh=99"
+      }
+
+
+    });
+
+    const options={
+      from : "gs2422163@gmail.com",
+      to :"gurkeeratsingh99@gmail.com",
+      subject:"You got question from "+name+ ", Email address: "+email,
+      text:reply
+    };
+
+    transporter.sendMail(options , function(err, info){
+      if(err){
+        console.log(err);
+        return;
+      }
+      console.log("Sent : " + info.response);
+    });
+
+res.sendFile(__dirname+"/index.html");
+
+});
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("The server is running at 3000 port")
