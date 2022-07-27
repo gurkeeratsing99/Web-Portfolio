6220e85de12d9b4c2778828e3af7e49b5b34a925
unknown <gurkeeratsingh99@gmail.com>
2022-07-27 16:42:33 -0700

    Ready with Spell check

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // AOS initialize
 AOS.init();
 AOS.init({
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-   offset: 120, // offset (in px) from the original trigger point
-   delay: 500, // values from 0 to 3000, with step 50ms
-   duration: 3000, // values from 0 to 3000, with step 50ms
-   easing: 'ease', // default easing for AOS animations
-   once: false, // whether animation should happen only once - while scrolling down
-   mirror: false, // whether elements should animate out while scrolling past them
-   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 70, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease-in-sine', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: true, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 });
